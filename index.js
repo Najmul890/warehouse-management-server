cc587acfd6a7b2b1564556644d208cebda81db53
Najmul890 <najmul890890@gmail.com>
2022-05-03 03:20:27 -0700

    get products api done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port= process.env.PORT || 5000;
 
@@ -11,15 +12,28 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pukwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log('db connected');
-  client.close();
-});
+
+
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db('iceStorage').collection('products');
+
+      //get all products
+      app.get('/products', async(req,res)=>{
+          const query={};
+          const cursor= productCollection.find(query);
+          const products= await cursor.toArray();
+          res.send(products)
+      })
+      
+    } finally {}
+      
+  }
+  run().catch(console.dir);
 
 
 app.get('/', (req,res)=>{
