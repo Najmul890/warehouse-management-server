72fd52c9af19bcdadab07a4b62468cdd54aa3846
Najmul890 <najmul890890@gmail.com>
2022-05-04 19:27:08 -0700

    delete api created to delete a product

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-const app= express();
- 
+const app = express();
+
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -19,59 +19,66 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
     try {
-      await client.connect();
-      const productCollection = client.db('iceStorage').collection('products');
+        await client.connect();
+        const productCollection = client.db('iceStorage').collection('products');
 
-      //get all products
-      app.get('/products', async(req,res)=>{
-          const query={};
-          const cursor= productCollection.find(query);
-          const products= await cursor.toArray();
-          res.send(products)
-      })
+        //get all products
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
 
-      //get a single product by id
-      app.get('/product/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query={_id: ObjectId(id)};
-        const product = await productCollection.findOne(query);
-        res.send(product);
-    });
+        //get a single product by id
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
 
-    // create a new product via post method
-    app.post('/product', async(req, res) =>{
-        const newProduct = req.body;
-        const result = await userCollection.insertOne(newProduct);
-        res.send(result)
-    });
+        // create a new product via post method
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        });
 
-    // update quantity for a product
-    app.put('/product/:id', async(req, res) =>{
-      const id = req.params.id;
-      const updatedProduct = req.body;
-      console.log(updatedProduct);
-      const filter = {_id: ObjectId(id)};
-      const options = { upsert: true };
-      const updatedDoc = {
-          $set: {
-              quantity: updatedProduct.quantity
-          }
-      };
-      const result = await productCollection.updateOne(filter, updatedDoc, options);
-      res.send(result);
+        // update quantity for a product
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body;
+            console.log(updatedProduct);
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedProduct.quantity
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
-  })
-      
-    } finally {}
-      
-  }
-  run().catch(console.dir);
+        // delete a product via id
+        app.delete('/product/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
+
+    } finally { }
+
+}
+run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('ice storage management server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
