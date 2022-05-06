b3af79a487f232d07d7221d7ad11803baad4e8b2
Najmul890 <najmul890890@gmail.com>
2022-05-06 06:35:49 -0700

    authentication via JWT done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,22 @@ app.use(cors());
 app.use(express.json());
 
 
+//verify token
+const verifyJWT=(req,res,next)=>{
+    const authHeader=req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+            return res.status(403).send({message: 'Forbidden Access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
+    
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pukwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -22,6 +39,17 @@ async function run() {
         await client.connect();
         const productCollection = client.db('iceStorage').collection('products');
 
+
+        //authentication post api
+        app.post('/login', async(req,res)=>{
+            const user=req.body;
+            const accessToken= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+               expiresIn: '1d' 
+            });
+            res.send({accessToken});
+        })
+
+
         //get all products
         app.get('/products', async (req, res) => {
             const query = {};
@@ -31,13 +59,18 @@ async function run() {
         })
 
         //get products for a specific user via user email
-        app.get('/myProducts', async (req, res) => {
+        app.get('/myProducts',verifyJWT, async (req, res) => {
+            const decodedEmail= req.decoded.email;
             const email=req.query.email;
-            console.log(email);
-            const query = {userEmail:email};
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products)
+            
+            if(email===decodedEmail){
+                const query = {userEmail:email};
+                const cursor = productCollection.find(query);
+                const products = await cursor.toArray();
+                res.send(products);
+            }else{
+                res.status(403).send({message: 'Forbidden Access'});
+            }
         })
 
         //get a single product by id
@@ -74,7 +107,6 @@ async function run() {
         // delete a product via id
         app.delete('/product/:id', async(req, res) =>{
             const id = req.params.id;
-            console.log(id);
             const query = {_id: ObjectId(id)};
             const result = await productCollection.deleteOne(query);
             res.send(result);
